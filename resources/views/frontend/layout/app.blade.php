4dd12f39f598963b16f015a0f8fed5aac1c033b7
abdulsami732 <abdulsami732@gmail.com>
2025-01-31 20:57:52 +0500

    Home page 2 sections

--- resources/views/frontend/layout/app.blade.php
+++ resources/views/frontend/layout/app.blade.php
@@ -5,7 +5,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @yield('meta_tags')
     <title>{{ config('app.name') }}</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
+        integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     @yield('styles')
 </head>
 
@@ -16,6 +22,11 @@
 
     @include('frontend.layout.footer')
 
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/js/all.min.js"
+        integrity="sha512-b+nQTCdtTBIRIbraqNEwsjB6UvL3UEMkXnhzd8awtCYh0Kcsjl9uEgwVFVbhoj3uu1DO1ZMacNvLoyJJiNfcvg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     @yield('scripts')
 </body>
 
--- resources/views/frontend/layout/header.blade.php
+++ resources/views/frontend/layout/header.blade.php
@@ -1,28 +1,32 @@
-{{-- header navbar html --}}
-{{-- 
-FRONTEND HREF LIST:
-1. Home: {{ route('home.index') }}
-2. How it works: {{ route('home.index') }}
-3. How is it free: {{ route('home.how-is-it-free') }}
-4. Replacement Vehicle: {{ route('home.replacement-vehicle') }}
-5. Accident Repairs: {{ route('home.accident-repairs') }}
---}}
-<nav>
-    <ul>
-        <li>
-            <a href="{{ route('home.index') }}">Home</a>
-        </li>
-        <li>
-            <a href="{{ route('home.index') }}">How it works</a>
-        </li>
-        <li>
-            <a href="{{ route('home.how-is-it-free') }}">How is it free</a>
-        </li>
-        <li>
-            <a href="{{ route('home.replacement-vehicle') }}">Replacement Vehicle</a>
-        </li>
-        <li>
-            <a href="{{ route('home.accident-repairs') }}">Accident Repairs</a>
-        </li>
-    </ul>
-</nav>
+<div class="header-top text-white py-2">
+        <div class="container d-flex justify-content-between align-items-center">
+            <span class="fw-bold">Call Now For Assistance: 0330 043 0036</span>
+        </div>
+    </div>
+    <header class="navbar navbar-expand-lg navbar-light bg-white py-3 shadow-sm">
+        <div class="container">
+            <a class="navbar-brand d-flex align-items-center" href="#">
+                <!-- <img src="logo.png" alt="MotorClaimsLine" style="height: 40px;"> -->
+                <h2 class="ms-2 fw-bold  mb-0">Swift Accident <span style="color: #007bff;">Solution</span></h2>
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav mx-auto">
+                    <li class="nav-item"><a class="nav-link fw-bold " href="#">How it works</a></li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link fw-bold  dropdown-toggle" href="#" id="servicesDropdown" role="button"
+                            data-bs-toggle="dropdown" aria-expanded="false">Our Services</a>
+                        <ul class="dropdown-menu" aria-labelledby="servicesDropdown">
+                            <li><a class="dropdown-item" href="#">Service 1</a></li>
+                            <li><a class="dropdown-item" href="#">Service 2</a></li>
+                        </ul>
+                    </li>
+                </ul>
+
+                <a href="#" class="btn fw-bold ms-3 claim-btn">Report a claim</a>
+            </div>
+        </div>
+    </header>
